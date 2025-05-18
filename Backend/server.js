5211a4740d9b4744d6ba85bff12ff99e471e0431
Mohit Kumar <mohitkumar@Mohits-MacBook-Pro.local>
2025-05-18 23:37:29 +0530

    pt2

--- Backend/server.js
+++ Backend/server.js
@@ -65,7 +65,7 @@ app.use(logRequest);
 // Rate limiting
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 10000, // Limit each IP to 100 requests per windowMs
   standardHeaders: true,
   legacyHeaders: false,
 });
